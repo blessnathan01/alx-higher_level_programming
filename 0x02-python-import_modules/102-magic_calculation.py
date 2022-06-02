876970ec23568d1df2c5cab77f2087cd6f01d4d5
blessnathan01 <blessingnathaniel01@gmail.com>
2022-06-02 16:30:32 -0700

    Add task 8 file once again

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import magic_calculation_102
+    if a < b:
+        c = a + b
+        for i in range(1, 90):
+            c += i
+        return c
+    return a - b
