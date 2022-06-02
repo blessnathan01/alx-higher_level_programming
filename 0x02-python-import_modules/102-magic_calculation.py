c4edb907de02a884ef527b1c7b8c3b63a60a9604
blessnathan01 <blessingnathaniel01@gmail.com>
2022-06-02 16:51:53 -0700

    Update task 8 file once again

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,7 +3,7 @@ def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
-        for i in range(1, 90):
+        for i in range(0, 90):
             c += i
         return c
     return sub(a, b)
