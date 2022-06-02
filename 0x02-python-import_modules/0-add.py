a7d9f7cdc64b035b76c7c17931d15cc20d3ff99f
blessnathan01 <blessingnathaniel01@gmail.com>
2022-06-02 12:38:33 -0700

    Update task 0 file once again

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-# Import a module
-from add_0 import add
+
+if __name__ == "__main__":
+    # Import a module
+    import add_0 as sum
 
 # Assign values to variables
 a = 1
 b = 2
 
 # print the result
-print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} + {} = {}".format(a, b, sum.add(a, b)))
