1435fece1af2913505657643b200c925df7c800c
blessnathan01 <blessingnathaniel01@gmail.com>
2022-06-06 14:23:23 -0700

    Update 8-multiple_returns.py once again

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == ():
+    if len(sentence) == 0:
         response = len(sentence), None
     else:
         response = len(sentence), sentence[0]
