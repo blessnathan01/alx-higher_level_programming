cbb6c2e9154628ab38d51645fa9e5b8ac603b2bf
blessnathan01 <blessingnathaniel01@gmail.com>
2022-06-08 18:28:12 -0700

    Update 100-weight_average.py once again

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def weight_average(my_list[]):
-    if my_list and len(my_list):
-        n = 0
-        d = 0
-        for tup in my_list:
-            n += (tup[0] * tup[1])
-            d += tup[1]
-        return (n / d)
+    if my_list:
+        wt = 0
+        deno = 0
+        avg = 0
+        for i in range(len(my_list)):
+            for j in range(len(my_list[i])):
+                wt += my_list[i][0] * my_list[i][1]
+                deno += my_list[i][1]
+        avg = wt / deno
+        return avg
     return 0
