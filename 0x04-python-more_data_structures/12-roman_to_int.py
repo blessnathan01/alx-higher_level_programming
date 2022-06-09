f235b7d73cbc16e82e1cbfc694ab412eae0b0699
blessnathan01 <blessingnathaniel01@gmail.com>
2022-06-08 18:16:44 -0700

    Update 12-roman_to_int.py once again and again

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-
-
 # roman_to_int - converts a roman numeral to an integer.
 def roman_to_int(roman_string):
-    romans = {'I': 1, 'V': 5, 'X': 10, 'L':50, 'C': 100, 'D': 500, 'M': 1000,\
-'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
-
+    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    romans['IV'] = 4
+    romans['IX'] = 9
+    romans['XL'] = 40
+    romans['XC'] = 90
+    romans['CD'] = 400
+    romans['CM'] = 900
     if roman_string and type(roman_string) == str:
         n = 0
         for i in range(len(roman_string)):
