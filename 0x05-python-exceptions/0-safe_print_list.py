31f730bb453e4c418f22648a6875fa7487299bd0
blessnathan01 <blessingnathaniel01@gmail.com>
2022-06-20 16:34:44 -0700

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for item in my_list[:x]:
             print("{}".format(item), end="")
             count += 1
-    except:
+    except BaseException:
         pass
     print()
     return count
