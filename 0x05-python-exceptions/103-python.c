964431b60a6e7b363dbf5d4810be225c7379239b
blessnathan01 <blessingnathaniel01@gmail.com>
2022-06-20 18:25:08 -0700

    Add two files of the last advanced task

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,40 @@
+#include "103-python.h"
+
+/**
+ * pylists - prints basic info about Python lists
+ * @p: pointer to the PyObject
+ *
+ * Return: nothing.
+ */
+void print_python_list(PyObject *p)
+{
+	PyObject *tmp;
+
+	setbuf(stdout, NULL);
+}
+
+/**
+ * pybytes - prints basic info about Python bytes
+ * @p: pointer to the PyObject
+ *
+ * Return: nothing.
+ */
+void print_python_bytes(PyObject *p)
+{
+        PyObject *tmp;
+
+	setbuf(stdout, NULL);
+}
+
+/**
+ * pyfloat - prints basic info about Python float
+ * @p: pointer to the PyObject
+ *
+ * Return: nothing.
+ */
+void print_python_float(PyObject *p)
+{
+        PyObject *tmp;
+
+	setbuf(stdout, NULL);
+}
---
+++ 0x05-python-exceptions/103-python.h
@@ -0,0 +1,11 @@
+#ifndef _103_PYTHON_H_
+#define _103_PYTHON_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
+#endif /* _103_PYTHON_H_ */
