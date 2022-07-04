731c6c66b65217143bae97636a15cdfc1951f798
blessnathan01 <blessingnathaniel01@gmail.com>
2022-07-04 16:49:47 -0700

    Add task 1 files

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,19 @@
+
+#!/usr/bin/python3
+"""
+    1-my_list: class MyList
+"""
+
+
+class MyList(list):
+    """
+        This class inherits from a list
+        Attributes:
+        Methods:
+            print_sorted - prints a sorted list
+    """
+    def print_sorted(self):
+        """
+           prints a list in ascending order
+        """
+        print(sorted(self))
