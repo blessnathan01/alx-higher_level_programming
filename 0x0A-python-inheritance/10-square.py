adf97795001974088fa05e81ab39932a13fdd447
blessnathan01 <blessingnathaniel01@gmail.com>
2022-07-04 17:33:28 -0700

    Add task 10 py file

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+    10-square: class Square from Rectangle
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+        Square inherits from Rectangle
+        Attributes:
+            size (int): side of the square
+        Methods:
+            __init__ - initializes a square
+    """
+    def __init__(self, size):
+        """
+            initializes the square
+        """
+        self.integer_validator("size", size)
+
+        self.__size = size
+
+    def area(self):
+        """
+            Returns the area of the square
+        """
+        area = self.__size * self.__size
+        return area
+
+    def __str__(self):
+        return ("[{}] {}/{}".format("Rectangle",
+                                    self.__size, self.__size))
