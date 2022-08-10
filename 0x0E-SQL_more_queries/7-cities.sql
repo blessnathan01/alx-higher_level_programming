90f79b93195d6f18b2cb9676947fadbf2cdb2979
blessnathan01 <blessingnathaniel01@gmail.com>
2022-08-10 16:31:27 -0700

    Add task 7 file

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,13 @@
+-- Script to create db, use it and
+-- create a table called cities
+-- CREATE DB
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+-- CREATE TABLE
+CREATE TABLE IF NOT EXISTS cities (id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL UNIQUE,
+state_id INT NOT NULL,
+name VARCHAR(256),
+INDEX(state_id),
+FOREIGN KEY (state_id) REFERENCES states(id));
