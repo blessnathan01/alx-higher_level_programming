85a55420f877c4b40b82d8945b9485fa3df4428b
blessnathan01 <blessingnathaniel01@gmail.com>
2022-08-10 16:14:14 -0700

    Add task 14 file

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- Script that lists all genres of Dexter show
+SELECT DISTINCT name FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY name ASC;
