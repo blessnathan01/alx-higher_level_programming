c478993729d4bfa7de242b63c62fab5869a5d1c2
blessnathan01 <blessingnathaniel01@gmail.com>
2022-08-10 16:16:59 -0700

    Add task 15 file

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,6 +1,6 @@
 -- Script that lists all shows in
 -- comendy genre in default order
--- by show titlw
+-- by show title
 SELECT title FROM tv_shows
 INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
