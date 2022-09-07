c92a2794bf317c62b3218c11c501583686378fb6
blessnathan01 <blessingnathaniel01@gmail.com>
2022-09-07 13:49:50 -0700

    Add task 6 file

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let x;
+for (x = 0; x <= 2; x++) {
+  console.log(languages[x]);
+}
